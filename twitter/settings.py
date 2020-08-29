81cfec8fd994703110af920fcfbf96a089fe83cd
rushil1999 <shahrushil1999@gmail.com>
2020-08-29 13:52:01 +0530

    Changed Database to postgresql chech the settings.py

--- twitter/settings.py
+++ twitter/settings.py
@@ -77,8 +77,16 @@ WSGI_APPLICATION = 'twitter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'twitterDB',
+        'USER' : 'rushil',
+        'PASSWORD' : '',
+        'HOST' : '',
+        'PORT' : '5432'
+
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
